a89b57bdd827eb6e8293e1be708bf5f6c82fc4e9
p-mcglaughlin <peter.mcglaughlin91@gmail.com>
2025-07-20 14:08:58 -0700

    Update page.py

--- wiki-reader/page.py
+++ wiki-reader/page.py
@@ -1,4 +1,7 @@
 class Page:
+    '''
+    struct for Wikipedia articles
+    '''
     def __init__(self, page_id: int, title: str, text: str):
         self.page_id = page_id
         self.title = title
